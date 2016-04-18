5fa90ed0a55af339fad0f667d27943dacc575dbd
jungej62 <jacob.jungers@gmail.com>
2016-04-18 16:18:56 -0500

    Added data file and added line to script to open.

--- IREE_Nstudy_script.R
+++ IREE_Nstudy_script.R
@@ -4,3 +4,6 @@ library(arm); library(coefplot); library(gridExtra); library(devtools)
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7","gray30")
 source_gist('https://gist.github.com/jungej62/5a76b72bcd0b12a7ce8a')
 #ssh: git@github.com:jungej62/IREE_Nstudy.git
+
+dat<-read.csv(file="IREE-N-All.csv", header=T)
+
