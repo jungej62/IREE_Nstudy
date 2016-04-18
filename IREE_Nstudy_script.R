c2512634e09ada21c7bfee2322eb21d191d64bc8
jungej62 <jacob.jungers@gmail.com>
2016-04-18 15:46:26 -0500

    The first commit

---
+++ IREE_Nstudy_script.R
@@ -0,0 +1,6 @@
+library(agricolae);library(nlme);library(car);library(faraway); library(ggplot2); library(lattice); library(reshape)
+library(multcomp); library(vegan); library(lme4); library(AICcmodavg);
+library(arm); library(coefplot); library(gridExtra); library(devtools)
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7","gray30")
+source_gist('https://gist.github.com/jungej62/5a76b72bcd0b12a7ce8a')
+#ssh: git@github.com:jungej62/IREE_Nstudy.git
